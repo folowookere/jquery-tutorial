931862cc4c8863ccf0ef09be07ab043d7e6fa447
Funmi <folowookere@yahoo.com>
2018-05-21 19:25:11 -0400

    Lessons 13-15

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,53 @@
-// Lesson 10
+//Lesson 13 - CSS with jQuery
+//Check the css value of a certain element
+// console.log($("#social-nav").css("top"));
 
-/*  wrap() - wraps all matched elements individually
-    unwrap() - unwraps all matched elements
-    wrapAll() - wraps all elements combined with 1 single element
-*/
-
-// $("section").wrap("<div>");
-// $("section").unwrap();
-// $("section").wrapAll("<div>");
-
-// var wrapper = "<div class ='wrapper'>";
-// var button = $(".button");
-// var wrapped = true;
-
-// button[0].onclick = function(){
-//     if (wrapped){
-//         $("section").unwrap();
-//         wrapped = false;
-//         button.text("wrap");
-//     }   else {
-//         $("section").wrapAll(wrapper);
-//         wrapped = true;
-//         button.text("unwrap");
-//     }
-// };
-
-//Lesson 11
-
-// $(".button").empty();
-// $("#points-of-sale").empty();
-
-// $(".button").remove();
-// $("#contact img").remove();
-
-// .empty() - empties the inner HTML of an element
-// .remove() - removes the element completely
-
-// Lesson 12
-
-console.log( $("#contact img").attr("alt"));
- 
-
+//Passing object into method for css, easier to maintain & manage than long chains
 /* 
-
-.removeAttr() removes an attribute completely
-.attr() can read or set any attribute
-
+$("#social-nav").css( {
+    "top" : "-400px",
+    "left" : "150",
+    "opacity" : "0.5",
+    "border-top" : "4px solid red"
+});
 */
 
+// Lesson 14 - Adding and Removing Classes
+
+var button = $("#lead-banner a");
+
+/* every time button "View our work" is clicked it toggles class open in css file
+button[0].onclick = function() {
+    $("#points-of-sale").toggleClass("open");
+    return false;  
+    this negates default behavior, button won't go anywhere
+};
+
+// $("header .wrapper").removeClass("wrapper");
+// $("header > div").addClass("wrapper");
 
 
+removeClass() - removes a class from the matched element(s)
+addClass() - adds a class to the matched element(s)
+toggleClass - toggles the class on and off on their matched element(s)
+*/
 
+// Lesson 15 - Binding & Unbinding Events
+
+
+var liPointsOfSale = $("#points-of-sale li");
+    liPointsOfSale.on("click", function(){
     
+    $(this).css({"background":"pink"});
+    
+    liPointsOfSale.off("click");
+    
+});
+
+/*
+
+on() - binds an event to matched element(s)
+off() - unbinds event from matched element(s)
+
+Site with types of events that can be listened for https://www.w3schools.com/jquery/jquery_ref_events.asp
+*/

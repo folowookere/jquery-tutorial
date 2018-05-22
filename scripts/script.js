43549f9765c58cbe3b969f9823697c3fca52a042
Funmi <folowookere@yahoo.com>
2018-05-22 19:22:09 -0400

    Lessons 22-24

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,62 @@
-//Lesson 19 - jQuery Animations
-
-// $(document).ready(function(){
-    
-//     function complete() {
-//         alert("Animation complete");
-//     }
-    
-//     $("section > h2").on("click", function(){
-        
-//         $(this).animate({"width" : "500px", "height" : "100px" }, 1000, "linear", complete);
-//         });
-        
-//     });
-    
-//Lesson 20 - Fading elements in & out
-
-// $(document).ready(function(){
-    
-    
-    
-//     $("section > h2").on("click", function(){
-        
-//         //fades out and removed from the document
-//         // $(this).fadeOut();
-//         // });
-        
-//         //  $(this).fadeOut(2000).fadeIn(500); //time in milliseconds
-//         // });
-        
-//          $(this)
-//             .fadeTo(200, 0.2)
-//             .fadeTo(200, 0.8)
-//             .fadeTo(200, 0.2)
-//             .fadeTo(200, 0.8)
-//             .fadeTo(200, 0.2)
-//             .fadeTo(200, 0.8);
-//          });
-        
-//     });
-    
-// Lesson 21 - Show, hide & toggle
+//Lesson 22 - Sliding elements
 
 $(document).ready(function(){
     
+    $(".slide-button-up").on("click", function(){
+        
+        //Toggles animations up or down
+        $("#lead-banner").slideToggle(1000, function(){
+            alert("Animation complete");
+        });
+        
+//         $("#lead-banner").slideUp(100);
+        
+
+// });
+//     $(".slide-button-down").on("click", function(){
+                
+//           $("#lead-banner").slideDown(4000, function(){
+//               alert("Animation complete");
+//         });
+    });
+});
+
+//Lesson 23 - Fading animations
+
+$(document).ready(function(){
     
+    var allQuotes = $("blockquote");
+    var currentQuote = 0;
     
-    $("img[alt=map]").on("click", function(){
+    function changeQuote() {
+        $(allQuotes[currentQuote]).fadeOut(500, function(){
+            
+             //If currentQuote is equal to 2 then go back to the first one
+          if(currentQuote == allQuotes.length - 1){
+            currentQuote = 0;
+        } else {
+            currentQuote++;
+        }
         
-        //Show and hide elements
-        // $(this).hide(1000).show(1000);
+        $(allQuotes[currentQuote]).fadeIn(500);
+        });
+      
+    }
+    
+    var quoteTimer = setInterval(changeQuote, 3000);
+    
+});
+
+//Lesson 24 - Sliding aninmation example
+
+$(document).ready(function(){
+    
+    var items = $("#points-of-sale li");
+    
+    items.on("click", function() {
         
-        //Toggle elements in and out. If it's not showing it will fade back in
-        $("section > h2").toggle(1000);
-        
-         });
+    $(this).find("p").slideToggle(500);
     });
     
-
-
-
-
+});
 

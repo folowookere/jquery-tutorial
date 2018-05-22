18eb54be5aaae9b3f64353aefb0b8126d6659800
Funmi <folowookere@yahoo.com>
2018-05-21 21:18:19 -0400

    Lessons 19-21

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,63 @@
-//Lesson 16 - Event helpers
+//Lesson 19 - jQuery Animations
 
-$("#lead-banner").on("dblclick", function() {
-    alert("You double clicked me");
-    $("#lead-banner").off("dblclick"); //Can only be clicked once
-});
-
-//Lesson 17 - Document Ready & Window Load Events
-//when scripts are at top of the page, or can use it just to make sure things are loaded first
-
-//when document is ready run what's inside
-
-// $(document).ready(function() {
+// $(document).ready(function(){
     
-// });
-
-// //or
-
-// $(function() {
+//     function complete() {
+//         alert("Animation complete");
+//     }
     
-// });
+//     $("section > h2").on("click", function(){
+        
+//         $(this).animate({"width" : "500px", "height" : "100px" }, 1000, "linear", complete);
+//         });
+        
+//     });
+    
+//Lesson 20 - Fading elements in & out
 
-// //window is ready
-
-// $(window).load(function() {
+// $(document).ready(function(){
     
     
     
-// });
-
-//Lesson 18 - The event object in jQuery 
-
-$(document).ready(function() {
+//     $("section > h2").on("click", function(){
+        
+//         //fades out and removed from the document
+//         // $(this).fadeOut();
+//         // });
+        
+//         //  $(this).fadeOut(2000).fadeIn(500); //time in milliseconds
+//         // });
+        
+//          $(this)
+//             .fadeTo(200, 0.2)
+//             .fadeTo(200, 0.8)
+//             .fadeTo(200, 0.2)
+//             .fadeTo(200, 0.8)
+//             .fadeTo(200, 0.2)
+//             .fadeTo(200, 0.8);
+//          });
+        
+//     });
     
-   $("*").on("click", function(e) {
-       console.log(e.target);
-       console.log("The event type is " + e.type);
-       console.log("The x coordinate of the event is: " + e.pageX);
-       console.log("The y coordinate of the event is: " + e.pageY);
-       e.stopPropagation();
-       
+// Lesson 21 - Show, hide & toggle
+
+$(document).ready(function(){
+    
+    
+    
+    $("img[alt=map]").on("click", function(){
+        
+        //Show and hide elements
+        // $(this).hide(1000).show(1000);
+        
+        //Toggle elements in and out. If it's not showing it will fade back in
+        $("section > h2").toggle(1000);
+        
+         });
     });
-});
+    
+
+
+
+
+
